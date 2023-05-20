7fb426766d3c26ebf23fef5e790ecdc723e7efa1
Sinyx <sinyx.contact@gmail.com>
2023-05-20 19:01:33 +0200

    fix: redirection

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,5 @@ import {Redirect} from "@docusaurus/router";
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
-  return <Redirect to={"/docs.s1nscripts.com/docs/intro/welcome"} />;
+  return <Redirect to={"/docs/intro/welcome"} />;
 }
